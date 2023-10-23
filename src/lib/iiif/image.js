1e360f1ce300abfd888680b9baf5a8ffa7bdc2e5
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2023-10-23 18:19:07 -0300

    Custom getRepresentativeImage

--- src/lib/iiif/image.js
+++ src/lib/iiif/image.js
@@ -1,14 +1,59 @@
-const { createThumbnailHelper } = require("@iiif/vault-helpers");
+//const { createThumbnailHelper } = require("@iiif/vault-helpers");
+const axios = require("axios");
 
-exports.getRepresentativeImage = async (canvas, preferredSize = 1200) => {
-  const helper = createThumbnailHelper();
+
+const getService = async (service, preferredWidth = 1200) => {
   try {
-    return await helper.getBestThumbnailAtSize(canvas, {
-      width: preferredSize,
-      height: preferredSize,
+    const response = await axios.get(`${service["id"]}/info.json`);
+    const { width, height } = response.data.sizes.reduce((a, b) => {
+      return Math.abs(b.width - preferredWidth) <
+        Math.abs(a.width - preferredWidth)
+        ? b
+        : a;
     });
-  } catch (error) {
-    console.log(error);
+    console.log('Width, height:', width, height)
+    return {
+      id: `${service["id"]}/full/${width},${height}/0/default.jpg`,
+      width: width,
+      height: height,
+    };
+  } catch (err) {
+    console.log(err.response.status);
     return {};
   }
 };
+
+exports.getRepresentativeImage = async (resource, preferredSize = 1200) => {
+  const firstCanvas = resource.items[0].items[0].items[0];
+  const firstCanvasService = await getService(
+    firstCanvas.body.service[0],
+    preferredSize
+  );
+  console.log('firstCanvasService:', firstCanvasService)
+  if (firstCanvasService) {
+    return {best:
+      {
+        id: firstCanvasService.id,
+        type: "Image",
+        format: "image/jpeg",
+        width: firstCanvasService.width,
+        height: firstCanvasService.height,
+      },
+    };
+  } else {
+    return resource.thumbnail ? resource.thumbnail : [];
+  }
+}
+
+// exports.getRepresentativeImage = async (canvas, preferredSize = 1200) => {
+//   const helper = createThumbnailHelper();
+//   try {
+//     return await helper.getBestThumbnailAtSize(canvas, {
+//       width: preferredSize,
+//       height: preferredSize,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     return {};
+//   }
+// };
